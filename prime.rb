968d2df209f95d6be6591d449127fc971c853c68
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-21 23:36:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,22 +1,9 @@
-# def prime?(num)
-#   return false if num < 2
-#   return true if num == 3 || n == 2
-#     if (2...n-1).any?{|i| n % i == 0}
-#       false
-#     else
-#       true
-#     end
-# end
-
-
 def prime?(num)
-  if num < 2
-    return false
-  elsif num == 3 || num == 2
-    return true
-  elsif (2..num).any?{|i| n % i == 0}
-    return false
-  else
-    return true
-  end
+  return false if num < 2
+  return true if num == 3 || num == 2
+    if (2...num-1).any?{|i| num % i == 0}
+      false
+    else
+      true
+    end
 end
